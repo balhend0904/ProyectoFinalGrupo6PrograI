13566e7c2bf0ef3c491ad80607b259bab12ebb48
Carlos Méndez Chinchilla <robert.menchi31@gmail.com>
2024-03-19 20:24:55 -0600

    Inicializacion de Repositorio

---
+++ src/proyectoprogramacioni/ProyectoProgramacionI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoprogramacioni;
+
+/**
+ *
+ * @author rober
+ */
+public class ProyectoProgramacionI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
